3dbb4cddf6dfdd75e316e02304780c21e7174a98
jan-woznica <129649904+jan-woznica@users.noreply.github.com>
2024-03-01 22:37:50 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,9 @@
+<!-- 
+querySelector označuje element a udržet to v proměnné myHeading
+a my heading nastavuje proměnnou pomocí textContent
+akorát mně to nějaká nefunguje xD 
+-->
 const myHeading = document.querySelector("h1");
 myHeading.textContent = "Hello world!";
+
+let myVariable;
