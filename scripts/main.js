764351202678809f0bbd72ec9611c16849a8b8e8
jan-woznica <129649904+jan-woznica@users.noreply.github.com>
2024-09-07 10:20:21 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,10 @@
+const myImage = document.querySelector("img");
 
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/billie.jfif") {
+    myImage.setAttribute("src", "images/kim.png");
+  } else {
+    myImage.setAttribute("src", "images/billie.jfif");
+  }
+};
