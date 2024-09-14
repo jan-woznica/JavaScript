25e8e6b29447be8f129b2a97fa71a7e52b21aadc
jan-woznica <129649904+jan-woznica@users.noreply.github.com>
2024-09-14 15:08:00 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
+/*img jsem uschoval v myImage*/
 const myImage = document.querySelector("img");
-
+/*Poté jsem vytvořil anonymní eventhandler onclick
+a kód vezme src a kontroluje, zda je rovna nebo ne*/
 myImage.onclick = () => {
   const mySrc = myImage.getAttribute("src");
   if (mySrc === "images/billie.jfif") {
@@ -8,3 +10,26 @@ myImage.onclick = () => {
     myImage.setAttribute("src", "images/billie.jfif");
   }
 };
+/*Personalizovaná zpráva - nyní vybere, kam to chceme směřovat*/
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+/*Nyní je funkce
+setusername funkce obsahuje prompt() funkci, které ukážw okno
+poté kód volá API localstorage, která umožnuje skladovat data a získat je později
+J8 používám localstorage seitem funkci, která vytvoří a uloží data jména pod názvem myName
+poté nastavíme texContent na string + jméno*/
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  localStorage.setItem("name", myName);
+  myHeading.textContent = `Mozilla is cool, ${myName}`;
+}
+/*Nyní je tu podmínkový blok po funkci, který inicializuje kód při prvním načtení
+první řádek obsahuje negační operátor NOT representovaný pomocí !, který kontrluje, zda data existují
+Pokud ne, tak běží funkce, Když ano, tak se vytáhne z local storage
+Data stáhneme pomocí getItem a nastavíme textcontent nadpisu na string+ jméno*/
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
